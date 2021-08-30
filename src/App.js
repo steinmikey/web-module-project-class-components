6e4ae961757c022ae2de0b2f7ad87ddabaac8467
Michael Steiner <michaelvsteiner@gmail.com>
2021-08-30 14:52:49 -0600

    Outline App.js with handles, and mock data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
-import React from 'react';
+import React from "react";
+
+import TodoList from "./components/TodoList";
+
+const initialToDos = [
+  {
+    task: "shopping",
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: "buy stock",
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: "sell civic",
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: "buy coke",
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: "water favorite plants",
+    id: Date.now(),
+    completed: false
+  }
+];
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { todoList: initialToDos };
+  }
+
+  handleAddTask = () => {
+    console.log("adding");
+  };
+
+  handleToggleComplete = () => {
+    console.log("toggling complete");
+  };
+
+  handleClearCompleted = () => {
+    console.log("clearing completed tasks");
+  };
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +55,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todoItems={this.state.todoList} />
       </div>
     );
   }
